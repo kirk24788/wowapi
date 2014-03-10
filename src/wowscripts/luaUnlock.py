b53aeac31c9b85db54c321bc224e88d97322dd63
Mario Mancino <mario@mancino-net.de>
2014-03-10 19:59:37 +0100

    fixed interact_unit added new lua-unlock - currently standalone - whoe api needs to be refactored..

--- src/wowscripts/luaUnlock.py
+++ src/wowscripts/luaUnlock.py
@@ -1,53 +1,154 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import argparse
+import time
+import os
+import sys
 import psutil
-from datetime import datetime
-from wowapi.helper import elevateSudo
-from wowapi.wow import WorldOfWarcraft
+try:
+    import pexpect
+except:
+    print "ERROR: Missing Python Library 'pexpect' - Please Install with 'sudo easy_install pexpect'"
+    sys.exit(3)
+import logging as LOG
+from subprocess import Popen, PIPE
 
-PIDS = WorldOfWarcraft.getAllPIDs()
 
-def getDescription():
-    description = "World of Warcraft LUA Unlocker:\n\n"
-    if len(PIDS) > 0:
-        key = 0
-        now = datetime.now()
-        for pid in PIDS:
-            p = psutil.Process(pid)
-            created = datetime.fromtimestamp(p.create_time)
-            ageInMinutes = (now-created).seconds % 60
-            description += ("%d (PID:%d) - Age: %d Minutes\n" % (key, pid,ageInMinutes))
-            key = key + 1
+TRIPWIRE = "0x0118bda8"
+LUA_UNLOCK_BP = "0x0080a0b0"
+LUA_UNLOCK_OFFSET = "0x159808c"
+CHARNAME_OFFSET = "0x0192fce4"
+
+LLDB_BINARY = "/usr/bin/lldb"
+
+def get_all_wow_apps():
+    output = Popen(["mdfind", "kMDItemCFBundleIdentifier == 'com.blizzard.worldofwarcraft'"], stdout=PIPE).communicate()[0]
+    return map(lambda x: "%s/Contents/MacOS/World of Warcraft" % x, filter(lambda x: x.endswith("Warcraft.app"), output.split("\n")))
+
+def get_all_wow_pids():
+    x86_binaries = get_all_wow_apps()
+    pids = []
+    for pid in psutil.get_pid_list():
+        try:
+            proc = psutil.Process(pid)
+            if(proc.exe in x86_binaries):
+                pids.append(pid)
+        except:
+            pass
+        #if(proc.exe in x86_binaries):
+        #    pids.append(pid)
+    return pids
+
+
+class LLDB():
+    def __init__(self,pid):
+        self.pid = pid
+        self.lldb = pexpect.spawn(LLDB_BINARY)
+        #self.lldb.logfile = sys.stdout
+        self._cmd("attach %d" % pid)
+        self._prepare_tripwire()
+        self._prepare_lua_unlock()
+        self._cmd("continue")
+       # self._cmd("script", expect=">>>")
+       # self._cmd("print 'OK OK OK!'; exit()", expect="print 'OK OK OK!'; exit\\(\\)")
+    
+    def _prepare_tripwire(self):
+        self._cmd("breakpoint set -a %s" % TRIPWIRE)
+        self._cmd("breakpoint command add -s python 1", expect="Type 'DONE' to end.")
+        self._cmd("""thread = frame.GetThread()
+process = thread.GetProcess()
+process.Detach()
+import sys
+print "IPC>>>WARNING: Tripwire activated - thread_get_state called - deactivating..."
+sys.exit(2)
+return False
+DONE""")
+
+    def _prepare_lua_unlock(self):
+        self._cmd("breakpoint set -a %s" % LUA_UNLOCK_BP)
+        self._cmd("breakpoint command add -s python 2", expect="Type 'DONE' to end.")
+        self._cmd("""thread = frame.GetThread()
+process = thread.GetProcess()
+error = lldb.SBError()
+addr = process.ReadUnsignedFromMemory(%s, 4, error)
+import sys
+if error.Success():
+    result = process.WriteMemory(addr,"\\x00\\x00\\x00\\x00", error)
+    if not error.Success() or result != 4:
+        process.Detach()
+        print 'IPC>>>ERROR: SBProcess.WriteMemory() failed!'
+        sys.exit(1)
+else:
+    process.Detach()
+    print "IPC>>>ERROR: Couldn't read LUA Unlock Address!"
+    sys.exit(1)
+return False
+DONE""" % LUA_UNLOCK_OFFSET)
+
+
+    def _cmd(self, cmd, expect="(lldb)"):
+        self.lldb.sendline(cmd)
+        if expect != None:
+            self.lldb.expect(expect)
+
+    def wait_for_exit(self):
+        is_running = True
+        while is_running:
+            try:
+                try:
+                    inp = self.lldb.read_nonblocking(100,1)
+                    if inp.startswith("IPC>>>"):
+                        print inp[6:]
+                    is_runnning = self.lldb.isalive()
+                except pexpect.EOF:
+                    self.lldb.close()
+                    is_running = False
+            except pexpect.TIMEOUT:
+                pass
+        return self.lldb.exitstatus
+
+    def detach(self):
+        self._cmd("detach")
+
+def print_usage():
+    print "Usage: %s <WOW_PID>" % sys.argv[0]
+    wow_pids = get_all_wow_pids()
+    if len(wow_pids)==0:
+        print "World of Warcraft not found - Please start it first! (64bit NOT supported)"
     else:
-        description += "NO ACTIVE WOW INSTANCES FOUND!"
-    return description
-
-def validateID(key):
-    key = int(key)
-    if len(PIDS) == 0:
-        raise argparse.ArgumentTypeError( 'No WoW Instances found!' )
-    if key >= 0 and key < len(PIDS):
-        return PIDS[key]
-    if len(PIDS)==1:
-        raise argparse.ArgumentTypeError( 'can only be 0' )
-    else:
-        raise argparse.ArgumentTypeError( 'has to be between 0 and %d' % (len(PIDS)-1) )
+        print "Available World of Warcraft PIDs:"
+        for pid in wow_pids:
+            print " * %d" % pid
 
 def main():
-    parser = argparse.ArgumentParser(description=getDescription(), formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('pid', metavar='INSTANCE KEY', type=validateID,  help='WoW Instance Key')
-    parser.add_argument('--revert', '-r', dest='revert', action='store_true', default=False, help='revert unlock?')
-    args = parser.parse_args()
-    elevateSudo()
-    w = WorldOfWarcraft(args.pid)
-    if args.revert:
-        print "Locking PID: %d" % args.pid
-        w.luaLock()
-    else:
-        print "Unlocking PID: %d" % args.pid
-        w.luaUnlock()
+    print "World of Warcraft LUA Unlocker (x86)"
+    osx_version = Popen(["sw_vers", "-productVersion"], stdout=PIPE).communicate()[0]
+    if not osx_version.startswith("10.9"):
+        print "ERROR: OSX Mavericks is required!"
+        sys.exit(1)
+    if not os.path.exists(LLDB_BINARY):
+        print "ERROR: Missing LLDB - Please Install Xcode Command Line Tools via 'xcode-select --install'"
+        sys.exit(1)
+    if len(sys.argv) != 2:
+        print_usage()
+        sys.exit(2)
+    try:
+        pid = int(sys.argv[1])
+    except ValueError:
+        print "ERROR: Invalid PID '%s' - must be a number!" % sys.argv[1]
+        print_usage()
+        sys.exit(2)
+    if pid not in get_all_wow_pids():
+        print "ERROR: %d is not a valid PID of a World of Warcraft Process!" % pid
+        print_usage()
+        sys.exit(2)
+    
+    try:
+        print "Unlocking %d..." % pid
+        lldb = LLDB(pid)
+        print "...LUA unlocked!"
+        lldb.wait_for_exit()
+    except KeyboardInterrupt:
+        print "\nExiting LUA Unlocker..."
+        lldb.detach()
+
 
 if __name__ == "__main__":
     main()
